32e87301cdda0c9e30e381611ab215d5eb52600b
adyanshaggarwal <adyanshaggarwal@gmail.com>
2025-07-14 12:14:05 +0530

    fixed profanity check

--- backend/preprocessor/profanity_check.py
+++ backend/preprocessor/profanity_check.py
@@ -1,1 +1,15 @@
-from profanity
+# pip install better-profanity
+
+from better_profanity import profanity
+profanity.load_censor_words()
+
+
+# input: whatever text
+# output: None if no profanity, else prints censored text and returns false 
+
+def check_profanity(text):
+    if profanity.contains_profanity(text):
+        print("Profanity detected!")
+        print(profanity.censor(text))  # You are a **** fool.
+        return 
+    return False
